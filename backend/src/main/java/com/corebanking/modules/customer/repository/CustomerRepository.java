3c1f736a0f2890e0d29b4ef344b321159dc51909
mariocodecr <arayaroyo686@gmail.com>
2026-05-12 17:30:07 -0600

    feat(customer): add countByEstado query for dashboard KPIs

--- backend/src/main/java/com/corebanking/modules/customer/repository/CustomerRepository.java
+++ backend/src/main/java/com/corebanking/modules/customer/repository/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.corebanking.modules.customer.repository;
 
 import com.corebanking.modules.customer.entity.Customer;
+import com.corebanking.modules.customer.entity.CustomerStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -14,4 +15,6 @@ public interface CustomerRepository extends JpaRepository<Customer, UUID>, JpaSp
     boolean existsByEmailIgnoreCase(String email);
 
     Optional<Customer> findByNumeroDocumento(String numeroDocumento);
+
+    long countByEstado(CustomerStatus estado);
 }
